f757e39169822c803f2429fc804fb25e26aaaced
Kevin-Rooseboom <rooseboomk@gmail.com>
2025-10-28 13:41:23 +0100

    Add properties Id, Title and Description.

--- TodoList.Core/Entities/Item.cs
+++ TodoList.Core/Entities/Item.cs
@@ -8,5 +8,8 @@ namespace TodoList.Core.Entities
 {
     public class Item
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 }
